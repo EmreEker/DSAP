ba3a445a10018acb776a5d4c7b1e72b3472158ec
Emre <icbm.emre@gmail.com>
2024-08-09 18:54:13 +0300

    list interface added

---
+++ src/com/emreeker/ds/list/List.java
@@ -0,0 +1,22 @@
+package com.emreeker.ds.list;
+
+public interface List<T> {
+
+	public void add(T element);
+	
+	public void addAll(List<T> elements);
+	
+	public boolean contains(T element);
+	
+	public void remove(int index);
+	
+	public int size();
+	
+	boolean isFull();
+	
+	boolean isEmpty();
+	
+	void reCreate();
+	
+
+}
